13a57aa974b0dd2fe3318de7a6af30edcd7b222d
Rowan Lindsay <rowan@rowanlindsay.com>
2024-02-28 02:33:07 -0800

    simplify parameter system

--- cucumber-playwright.ts
+++ cucumber-playwright.ts
@@ -58,22 +58,16 @@ After({ name: "shut down playwright" }, async function (this: CustomWorld) {
 
 // the define step function in cucumber is deprecated, but we redefine it here to use the
 // same logic for all step definition functions
-// TODO infer type of params
-function defineStep(pattern: DefineStepPattern, paramTypes: ZodType[], code: (args: PlaywrightTestArgs & F & { world: T }, ...params: any[]) => any | Promise<any>, options: IDefineStepOptions = {}) {
+// TODO: figure out how to get type safety
+function defineStep(pattern: DefineStepPattern, numParams: number, code: (args: PlaywrightTestArgs & F & { world: T }, ...params: any[]) => any | Promise<any>, options: IDefineStepOptions = {}) {
   const runUserCode = async (world: CustomWorld, ...params: unknown[]) => {
-    paramTypes.forEach((type, i) => {
-      const typeResult = type.safeParse(params[i]);
-      if (!typeResult.success) {
-        throw new Error(`Incorrect parameter type at index ${i}`);
-      }
-    });
     return await code({
       ...world.builtInFixtures,
       ...world.userFixtures,
       world: world.userWorld
     }, ...params);
   }
-  const paramArgs = paramTypes.map((_, i) => `param${i}`);
+  const paramArgs = Array.from(Array(numParams).keys()).map((i) => `param${i}`);
   const handler = new Function(
     "fn",
     `return async function (${paramArgs.join(",")}) { return await fn(${["this", ...paramArgs].join(",")}) }`,
--- features/support/steps.ts
+++ features/support/steps.ts
@@ -5,24 +5,24 @@ import { string } from "zod";
 
 const { defineStep, Then, When } = registerCucumberPlaywright<{ foo: number }, Fixtures>(() => ({ foo: 1 }), fixtures);
 
-When("On the playwright page", [], async ({ page, world: { foo } }) => {
+When("On the playwright page", 0, async ({ page, world: { foo } }) => {
   await page.goto("https://playwright.dev/");
 });
 
-Then("The title {string} should be displayed", [string()], async ({ page }, title) => {
+Then("The title {string} should be displayed", 1, async ({ page }, title: string) => {
   await expect(page).toHaveTitle(title);
 });
 
-When("The getting started button is clicked", [], async ({ page }) => {
+When("The getting started button is clicked", 0, async ({ page }) => {
   await page.getByRole("link", { name: "Get started" }).click();
 });
 
-Then("The installation link should be displayed", [], async ({ page }) => {
+Then("The installation link should be displayed", 0, async ({ page }) => {
   await expect(
     page.getByRole("heading", { name: "Installation" })
   ).toBeVisible();
 });
 
-defineStep("Get custom fixture info", [], async ({ myFixture }) => {
+defineStep("Get custom fixture info", 0, async ({ myFixture }) => {
   expect(myFixture).toEqual("hello")
 });
