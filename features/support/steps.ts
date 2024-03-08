a1fbfdc0a99a530c28088d50dd62ba283d4eeda9
Rowan Lindsay <rowan@rowanlindsay.com>
2024-03-07 23:51:57 -0800

    format steps

--- features/support/steps.ts
+++ features/support/steps.ts
@@ -8,15 +8,21 @@ type WorldType = {
 
 const initialWorld: WorldType = { myCount: 0 };
 
-const { Then, When } = registerCucumberPlaywright<WorldType, Fixtures>(() => initialWorld, fixtures);
+const { Then, When } = registerCucumberPlaywright<WorldType, Fixtures>(
+  () => initialWorld,
+  fixtures
+);
 
 When("On the playwright page", async ({ page, world }) => {
   await page.goto("https://playwright.dev/");
 });
 
-Then("The title {string} should be displayed", async ({ page }, title: string) => {
-  await expect(page).toHaveTitle(new RegExp(title));
-});
+Then(
+  "The title {string} should be displayed",
+  async ({ page }, title: string) => {
+    await expect(page).toHaveTitle(new RegExp(title));
+  }
+);
 
 When("The getting started button is clicked", async ({ page }) => {
   await page.getByRole("link", { name: "Get started" }).click();
@@ -28,17 +34,21 @@ Then("The installation link should be displayed", async ({ page }) => {
   ).toBeVisible();
 });
 
-Then('make an API request', async ({ myCustomRequestContext }) => {
-  const response = await myCustomRequestContext.get('https://jsonplaceholder.typicode.com/todos/1');
-  expect(await response.json()).toEqual(expect.objectContaining({
-    userId: 1
-  }));
-})
+Then("make an API request", async ({ myCustomRequestContext }) => {
+  const response = await myCustomRequestContext.get(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
+  expect(await response.json()).toEqual(
+    expect.objectContaining({
+      userId: 1,
+    })
+  );
+});
 
-When('the count is incremented', ({ world }) => {
+When("the count is incremented", ({ world }) => {
   world.myCount++;
 });
 
-Then('the count is {int}', ({ world: { myCount } }, count: number) => {
+Then("the count is {int}", ({ world: { myCount } }, count: number) => {
   expect(myCount).toEqual(count);
 });
